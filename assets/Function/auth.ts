8dc545346e1206b748812a30101528ed28bc2c1d
Eroiko <shingekinocore@gmail.com>
2023-05-27 03:50:27 +0800

    Define data structure to communicate with firebase

--- assets/Function/auth.ts
+++ assets/Function/auth.ts
@@ -4,7 +4,8 @@ export type UserInfo = {
     life: number,
     coin: number,
     score: number,
-    time: number
+    time: number,
+    stage: number,
 }
 
 export function getDefaultUserInfo(email: string, username: string): UserInfo {
@@ -14,7 +15,8 @@ export function getDefaultUserInfo(email: string, username: string): UserInfo {
         life: 5,
         coin: 0,
         score: 0,
-        time: 300
+        time: 300,
+        stage: 0,
     }
 }
 
@@ -31,7 +33,7 @@ export let userInfo: {
  * @param newInfo new userInfo object
  */
 export function updateUserInfo(newInfo: UserInfo) {
-    userInfo.info = newInfo;
+    userInfo.info = {...userInfo.info, ...newInfo};
     for (let fn of userInfo.callbacks)
         fn(userInfo.info);
 }
@@ -44,10 +46,89 @@ export function addUserInfoUpdatedCallback(callback: (info: UserInfo) => void) {
     userInfo.callbacks.push(callback);
 }
 
+/**
+ * Append callback function for userInfo
+ * @param callback callback function to be called after each update of userInfo
+ */
+export function removeUserInfoUpdatedCallback(callback: (info: UserInfo) => void) {
+    userInfo.callbacks = userInfo.callbacks.filter(f => f != callback);
+}
+
 export function email2uid(email: string) {
     return email.replace(/@/g, '_').replace(/\./g, '=')
 }
 
 export function uid2email(uid: string) {
     return uid.replace(/=/g, '.').replace(/_/g, '@')
+}
+
+export type RankItem = {
+    uid: string,
+    username: string,
+    score: number,
+}
+
+/**
+ * Update this using updateRankList function
+ */
+export let rankList: {
+    list: RankItem[] | null,
+    callbacks: ((newList: RankItem[]) => void)[]
+} = {
+    list: null,
+    callbacks: []
+};
+
+export function updateRankList(newItem: RankItem, needUpdate = true) {
+    if (!rankList.list)
+        rankList.list = new Array();
+    if (rankList.list.includes(newItem))
+        return;
+    let toCallback = false;
+    for (let item of rankList.list) {
+        if (item.uid === newItem.uid) {
+            if (newItem.score > item.score) {
+                item.score = newItem.score;
+                firebase.database().ref(`scores/${newItem.uid}`)
+                    .update(newItem);
+                toCallback = true;
+            } else {
+                return;
+            }
+        }
+    }
+    if (!toCallback) {
+        let newList = rankList.list.concat(newItem).sort((a, b) => b.score - a.score);
+        if (newList.length <= 5 || newList[0].uid !== newItem.uid) { // need update
+            rankList.list = newList;
+            if (needUpdate) {
+                // update new score
+                firebase.database().ref(`scores/${newItem.uid}`)
+                    .update(newItem);
+                if (newList.length > 5) {
+                    // delete lowest score
+                    firebase.database().ref(`scores/${newList[0].uid}`)
+                        .update(null);
+                }
+            }
+        }
+    }
+    for (let fn of rankList.callbacks)
+        fn(rankList.list);
+}
+
+/**
+ * Append callback function for userInfo
+ * @param callback callback function to be called after each update of userInfo
+ */
+export function addRankListUpdatedCallback(callback: (newList: RankItem[]) => void) {
+    rankList.callbacks.push(callback);
+}
+
+/**
+ * Append callback function for userInfo
+ * @param callback callback function to be called after each update of userInfo
+ */
+export function removeRankListUpdatedCallback(callback: (newList: RankItem[]) => void) {
+    rankList.callbacks = rankList.callbacks.filter(f => f != callback);
 }
--- assets/Function/database.ts
+++ assets/Function/database.ts
@@ -1,4 +1,4 @@
-import { UserInfo, addUserInfoUpdatedCallback, updateUserInfo, userInfo } from "./auth";
+import { RankItem, UserInfo, addUserInfoUpdatedCallback, rankList, updateRankList, updateUserInfo, userInfo } from "./auth";
 
 export function firebaseSetUserInfo(uid: string): Promise<void> {
     return firebase.database().ref(`users/${uid}`).set(userInfo.info);
@@ -6,14 +6,14 @@ export function firebaseSetUserInfo(uid: string): Promise<void> {
 
 export function firebaseOnUpdateUserInfo(uid: string) {
     firebase.database().ref(`users/${uid}`)
-    .on('value', (snapshot) => {
-        if (snapshot.exists()) {
-            cc.log('Load from firebase');
-            updateUserInfo(snapshot.val());
-        } else {
-            console.log("No data available");
-        }
-    })
+        .on('value', (snapshot) => {
+            if (snapshot.exists()) {
+                cc.log('Load from firebase');
+                updateUserInfo(snapshot.val());
+            } else {
+                console.log("No data available");
+            }
+        })
 }
 
 export function firebaseAssignUpdateCallback(uid: string) {
@@ -25,7 +25,20 @@ export function firebaseAssignUpdateCallback(uid: string) {
     addUserInfoUpdatedCallback(updateToFirebase);
 }
 
-export function firebaseUpdateScore(uid: string, newScore: number) {
-    firebase.database().ref(`scores/${uid}`)
-        .update({ score: newScore });
+export function firebaseOnUserInfoUpdateRankList() {
+    firebase.database().ref(`scores/`)
+        .on('value', (snapshot) => {
+            if (snapshot.exists()) {
+                snapshot.forEach(child => {
+                    updateRankList({
+                        uid: child.key,
+                        username: child.val().username,
+                        score: child.val().score
+                    }, false);
+                })
+                cc.log('Load rankList from firebase');
+            } else {
+                console.log("No list available");
+            }
+        })
 }
